b8535d57fd046516400d0e64061ab6690cf78697
dmitry4567 <dimon2003pp@gmail.com>
2026-07-21 20:54:51 +0300

    fixes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // База './' — чтобы собранный сайт открывался из любой папки (в т.ч. как статические файлы).
 export default defineConfig({
-  base: '/pilyom-valim-react_portfolio/',
+  base: '/',
   plugins: [react()],
 })
